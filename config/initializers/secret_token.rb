1a8fd883a7d64b55499840588102bf567fd10d5d
Jayanth <jayanthgithub@gmail.com>
2013-07-21 18:22:48 -0700

    The initial directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PNGFeed::Application.config.secret_key_base = 'f380baba488ab03679facbfd33b94bf68f12b19bf73e2f14a30359036121645395f5d58cadba663d6d86beb682c1a5bfc6903abbc71a28c87ee1ac7ecaaa44c6'
